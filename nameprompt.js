81d1d4b933b9cebcafbb0ebe07b9ba53ad7d91fb
NoGameNoLife <mushroom8520@gmail.com>
2019-11-09 19:26:10 +0800

    第一次上傳

---
+++ nameprompt.js
@@ -0,0 +1,7 @@
+alert("Hello World!");
+var userFirstName = prompt("what is your first name?");
+var userLastName = prompt("what is your last name?");
+var age = prompt("Hw old are you?");
+console.log("Nice to meet you, " + userFirstName + " " + userLastName);
+console.log("and your age is " + age);
+
