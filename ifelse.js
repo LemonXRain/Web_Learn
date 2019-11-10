993fa3e55029d0819e1ad9829cb5b00ec0dc4b09
Maisie <happy50836@gmail.com>
2019-11-10 17:38:11 +0800

    if else learning

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+var age = prompt ("How old are you?");
+if (age<0) {
+    console.log("Error MSG");
+}
+if (age==21) {
+    console.log("happy 21th birthday");
+}
+if (age%2) {
+    console.log("your age is odd");
+}
+if (Math.sqrt(age)) {
+    console.log("Perfect Square");
+}
