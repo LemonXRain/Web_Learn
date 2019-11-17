f5690a08a4588507ca93f49eb8874536e4f510ef
Maisie <happy50836@gmail.com>
2019-11-17 23:17:45 +0800

    printReverse

--- isEven.js
+++ isEven.js
@@ -1,10 +1,33 @@
 //function isEven(num) {
 //return (num % 2) == 0;}
 
-function factorial(num) {
-    if (num === 1) {
-        return num
-    } else {
-        return num * factorial(num - 1)
+// function factorial(num) {
+// if (num === 1) {
+// return num} 
+// else {
+// return num * factorial(num - 1)}}
+
+// 取反向陣列，不改變原陣列
+//*** printReverse([1,2,3,4]) ***
+
+function printReverse(num) {
+
+    for (var count = num.length; 0 < count; count--) {
+        console.log(num[count - 1]);
     }
 }
+printReverse([1, 2, 3, 4]);
+
+// 判斷陣列中的值是否都相同
+//*** isUniform([1,1,1,1]) ***
+
+
+
+
+
+// 把陣列中的元素都相加
+// *** sumArray() ***
+
+
+// 取陣列中的最大值
+// *** max() ***
