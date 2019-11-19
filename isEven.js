3c91239aff58288c9e1a5584abeb461847748cc6
Maisie <happy50836@gmail.com>
2019-11-19 18:24:51 +0800

    homework

--- isEven.js
+++ isEven.js
@@ -21,13 +21,23 @@ printReverse([1, 2, 3, 4]);
 // 判斷陣列中的值是否都相同
 //*** isUniform([1,1,1,1]) ***
 
+function isUniform(arr) {
+    var first = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] != first) {
+            return false;
+        }
+    }
+    return true;
 
-
-
+}
+isUniform([1, 1, 1, 1]);
 
 // 把陣列中的元素都相加
-// *** sumArray() ***
+// *** sumArray([1,2,3,4]) ***
+
 
 
 // 取陣列中的最大值
-// *** max() ***
+// *** sumArray([1,2,3,4]) ***
+
